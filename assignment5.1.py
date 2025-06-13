070edb3e58adc67ab895aa7268f69ace37a60da1
vaibhavavdhut001 <vaibhavavdhut001@gmail.com>
2025-06-13 10:33:16 +0530

    Add files via upload

---
+++ assignment5.1.py
@@ -0,0 +1,4 @@
+d={'Alice':85, 'Ram':50}
+
+i= input("Enter the student's name: ")
+print(d.get(i,"Student not found."))
---
+++ assignment5.2'.py
@@ -0,0 +1,6 @@
+l=list(range(1,11))
+e= l[0:5]
+r = list(reversed(e))
+
+print("Original list:{} \nExtracted first five elements:{} \nReversed extracted elements:{}".format(l,e,r))
+
